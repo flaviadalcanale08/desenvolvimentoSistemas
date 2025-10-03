2438e06b69f89d30188c5d72f3cc3fc05efbefb9
flaviadalcanale08 <4553692090@estudante.sed.sc.gov.br>
2025-10-03 11:21:11 -0300

    Update Aula03-10.py

--- Aula03-10.py
+++ Aula03-10.py
@@ -1,3 +1,4 @@
+
 def comparador_de_numeros(numero1, numero2):
 
     if numero1 > numero2 :
@@ -8,9 +9,16 @@ def comparador_de_numeros(numero1, numero2):
      return 'Os dois números são iguais.'
 
 
+import unittest
 
+from Flavia203Aula03_10 import *
+class testeCompleto(unittest.TestCase):
 
     def testar_comparador_de_numeros(self):
         self.assertEqual(comparador_de_numeros(3, 2), '3 é maior do que o segundo número.')
         self.assertEqual(comparador_de_numeros(10, 11), '11 é maior do que o primeiro número.')
         self.assertEqual(comparador_de_numeros(10, 10), 'Os dois números são iguais.')
+
+
+if __name__ == "__main__" :
+    unittest.main()
