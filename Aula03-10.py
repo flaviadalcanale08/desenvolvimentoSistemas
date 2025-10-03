0b980feb445115924cdc9e99652c5feec8897f4b
flaviadalcanale08 <4553692090@estudante.sed.sc.gov.br>
2025-10-03 11:17:16 -0300

    Create Aula03-10.py

---
+++ Aula03-10.py
@@ -0,0 +1,16 @@
+def comparador_de_numeros(numero1, numero2):
+
+    if numero1 > numero2 :
+     return (f'{numero1} é maior do que o segundo número.') 
+    if numero2 > numero1 :
+     return (f'{numero2} é maior do que o primeiro número.')
+    if numero1 == numero2 : 
+     return 'Os dois números são iguais.'
+
+
+
+
+    def testar_comparador_de_numeros(self):
+        self.assertEqual(comparador_de_numeros(3, 2), '3 é maior do que o segundo número.')
+        self.assertEqual(comparador_de_numeros(10, 11), '11 é maior do que o primeiro número.')
+        self.assertEqual(comparador_de_numeros(10, 10), 'Os dois números são iguais.')
